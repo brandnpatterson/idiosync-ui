520e170d55336d3a900b427bda062745e35dd49d
Brandon Patterson <brandnpatterson@gmail.com>
2017-08-27 16:50:22 -0500

    set up tags for the view on tags page

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -22,74 +22,62 @@ const Tags = ({ articles, tags }) => {
         return first
       }, [])
       tags = reducedTags
+
       return tags
     })
   })
 
   return (
-    <TableOfContents>
-      <div className="outer">
-        <h2 className="title">Idiosync</h2>
-        <div className="inner">
-          <h2>Articles</h2>
-          <hr />
-          {
-            tags.map(tag => (
-              <ul key={tag.id}>
-                <li className="title">
-                  <Link to={`/tags/${tag.id}`}>{tag.name}</Link>
-                </li>
-              </ul>
-            ))
-          }
-        </div>
+    <TagsDiv>
+      <h2>Tags</h2>
+      <ul>
+        {
+          tags.map(tag => (
+            <li key={tag.id} className="title">
+              <Link to={`/tags/${tag.id}`}>{tag.name}</Link>
+            </li>
+          ))
+        }
+      </ul>
+      <div className="inner">
+        <h2>Articles</h2>
+        <hr />
+        {
+          articles.map(article => (
+            <ul key={article.id}>
+              <li className="author">
+                {article.author}
+              </li>
+              <li className="title">
+                <Link to={`/articles/${article.id}`}>{article.title}</Link>
+              </li>
+            </ul>
+          ))
+        }
       </div>
-    </TableOfContents>
+    </TagsDiv>
   )
 }
 
 Tags.propTypes = {
-  articles: array.isRequired
+  articles: array.isRequired,
+  tags: array.isRequired
 }
 
 // style
-const TableOfContents = styled.div `
+const TagsDiv = styled.div `
   background: whitesmoke;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1), 0 5px 10px rgba(0, 0, 0, 0.05);
   margin: 5em auto;
   max-width: 50em;
   width: 80%;
-  .outer {
-    padding: 2em 0;
-    h2.title {
-      font-size: 2.3em;
-      text-align: center;
-    }
-    .inner {
-      margin: 5em auto;
-      max-width: 30em;
-      width: 80%;
-      h2,
-      hr {
-        margin: 0;
-      }
-      ul {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        padding: 0;
-        li {
-          margin: 0 1em;
-          width: 100%;
-        }
-        .author {
-          text-align: right;
-        }
-        .title {
-          font-style: italic;
-          text-align: left;
-        }
-      }
+
+  ul {
+    display: flex;
+    justify-content: center;
+
+    li {
+      margin: 0 15px;
     }
   }
 `
